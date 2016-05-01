a221678783a2538bf053fc2b9f7ca8d87fba7a70
zzttyybb <329424435@qq.com>
2016-05-01 20:54:48 +0800

    2

--- src/com/atguigu/helloworld/hellworld.java
+++ src/com/atguigu/helloworld/hellworld.java
@@ -5,7 +5,8 @@ public class hellworld {
 	public static void main(String[] args) {
 		System.out.println("github hello 01,第一次request请求");
 		System.out.println("github hello 02,第二次request请求");
-		System.out.println("github收到第一次响应");
+		System.out.println("github, 回应第一个request");
+		System.out.println("github,回应第二个request");
 
 	}
 	
