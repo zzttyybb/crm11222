b0718f3a514881ddd46d738969126bd852d7c614
zzttyybb <329424435@qq.com>
2016-05-01 10:21:15 +0800

    update 01

--- src/com/atguigu/helloworld/hellworld.java
+++ src/com/atguigu/helloworld/hellworld.java
@@ -3,6 +3,7 @@ package com.atguigu.helloworld;
 public class hellworld {
 
 	public static void main(String[] args) {
-		System.out.println("github hello 01");
+		System.out.println("github hello 01,第一次request请求");
+		System.out.println("github hello 02,第二次request请求");
 	}
 }
